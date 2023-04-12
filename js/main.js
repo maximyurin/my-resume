01a47ed0385f634431578bcbc109a0834318ea7a
Maxim <maximyurinmusic@gmail.com>
2023-04-12 22:53:00 +0300

    Home work in process

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ const paragraphs = {
   b: document.querySelector(".person-skills"),
   c: document.querySelector(".person-work-experience"),
   d: document.querySelector(".person-education"),
+  p: document.querySelector(".person-portfolio"),
 };
 
 document.addEventListener("keydown", (event) => {
@@ -35,4 +36,43 @@ document.addEventListener("keydown", (event) => {
   if (paragraphs[key]) {
     paragraphs[key].scrollIntoView({ block: "start", behavior: "smooth" });
   }
+});
+
+/*
+Додати список своїх репозиторіїв з github у своє резюме:
+1. Додати в сторінку-резюме секцію “Мої проєкти”.
+2. Додати в скрипт подію на завантаження документа.
+В функції-обробнику події зробити запит на https://api.github.com/users/ <Ваш логін в github>/repos
+ та отримати список ваших репозиторіїв.
+3. З відповіді використовувати наступні поля full_name, html_url, description.
+4. Вивести отримані дані на сторінку в додану секцію з п1. Показувати посилання на репозиторій,
+де full_name текст посилання, а html_url – адреса посилання, під посиланням показати description,
+але тільки за умови, що description не є пустим.
+ */
+
+const container = document.querySelector(".person-portfolio section");
+window.addEventListener("load", () => {
+  async function getMyPortfolio() {
+    const portfolioList = await fetch(
+      "https://api.github.com/users/maximyurin/repos"
+    );
+    const list = await portfolioList.json();
+    list.forEach((repository) => {
+      const link = document.createElement("a");
+      link.setAttribute("href", `${repository.html_url}`);
+      link.classList.add("repository-link");
+      link.setAttribute("target", "_blank");
+      link.innerHTML = `${repository.full_name}`;
+      container.appendChild(link);
+      if (repository.description) {
+        const repositoryDescription = document.createElement("p");
+        const name = repository.full_name.replace("maximyurin/", "");
+        repositoryDescription.classList.add("repository-description");
+        repositoryDescription.innerHTML = `<strong>Description for ${name}</strong>: ${repository.description}`;
+        container.appendChild(repositoryDescription);
+      }
+      return repository;
+    });
+  }
+  getMyPortfolio();
 });
